03a8c037c7551bdc519f2df389384df2f0c453a3
Telma gama <telmagamasilva@gmail.com>
2018-06-02 00:27:08 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoDeRepeticaoFor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int linha = 0; linha < 10; linha++)
+            {
+                for(int coluna = 0; coluna < 10; coluna++)
+                {
+                    if( coluna > linha)
+                    {
+                        break;
+                    }
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
