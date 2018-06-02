d34782a87b98a0bf420e5ded633eecbd788bd5e5
Telma gama <telmagamasilva@gmail.com>
2018-06-02 00:12:51 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._1_lacosDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int contador= 1; contador <= 10; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
