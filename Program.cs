5321cacc9587520c5294a2bbbe7264516688dc48
Telma gama <telmagamasilva@gmail.com>
2018-06-02 00:25:21 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoDeRepeticaoFor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                for(int contador = 0; contador <= 10; contador++)
+                {
+                    Console.Write(multiplicador * contador);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
